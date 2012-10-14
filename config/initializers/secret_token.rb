b884dd4bad9a3e71be5f64509417e6c57037fdf9
Michael Skibo <mskibo@mac.com>
2012-10-14 16:41:37 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AddressAp::Application.config.secret_token = '826734df9c9ec4a0e50b1ec732e35e246add5bddf9edd3a72f1d0932b8d59b04715feda7e6f5e58e6ce3d13257a0697f37c430cf6e37b9772a847387398c0744'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AddressAp::Application.config.session_store :cookie_store, key: '_AddressAp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AddressAp::Application.config.session_store :active_record_store
